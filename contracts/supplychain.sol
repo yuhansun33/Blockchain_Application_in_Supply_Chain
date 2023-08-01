86f3c1c3ab375a6c58c34da5b4a10da7a24089ec
Linszuchi047 <l109704020.mg09@nycu.edu.tw>
2023-08-01 10:07:50 +0800

    update

---
+++ contracts/supplychain.sol
@@ -0,0 +1,53 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.22 <0.9.0;
+
+enum ProductState{
+    SETTING,
+    PROCESSING,
+    FINISH,
+    IN_TRANSIT,
+    ARRIVED
+}
+
+contract SupplyChain{
+    struct Product{
+        address ProductID;
+        string ProductName;
+        string ProductType;
+        uint ProductPrice;
+        ProductState state;
+        
+}
+mapping (address => Product) myproduct;
+address[] public ProductAddresses;
+
+function createProduct(string memory _ProductName,string memory _ProductType, uint _ProductPrice) public {
+    address addr = address(bytes20(sha256(abi.encodePacked(msg.sender,block.timestamp))));
+    myproduct[addr].ProductID = addr;
+    myproduct[addr].ProductName = _ProductName;
+    myproduct[addr].ProductType = _ProductType;
+    myproduct[addr].ProductPrice = _ProductPrice;
+    myproduct[addr].state = ProductState.SETTING;
+
+    ProductAddresses.push(addr);
+}
+
+function getAllProductAddr() external view returns (address[] memory) {
+    return ProductAddresses;
+}
+
+
+function getProduct(address id) public view returns (address adrr,string memory _ProductName,string memory _ProductType, uint _ProductPrice,ProductState _state)
+    {
+       
+        Product memory p = myproduct[id];
+        
+        
+        return (p.ProductID, p.ProductName, p.ProductType, p.ProductPrice,p.state);
+    }
+
+}
+
+
+
