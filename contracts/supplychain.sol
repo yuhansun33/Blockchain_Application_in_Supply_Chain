0686a8c59dc38f47e2f8ae729f5999d5f7312e4c
jocelyn1105 <weiling.mg09@nycu.edu.tw>
2023-08-08 16:29:31 +0800

    改了一下名字的形式

--- contracts/supplychain.sol
+++ contracts/supplychain.sol
@@ -19,36 +19,36 @@ contract SupplyChain{
         uint ProductPrice;
         ProductState state;
         
-}
-mapping (address => Product) myproduct;
-address[] public ProductAddresses;
-
-function createProduct(string memory _ProductName,string memory _ProductType, uint _ProductPrice) public {
-    address addr = address(bytes20(sha256(abi.encodePacked(msg.sender,block.timestamp))));
-    myproduct[addr].ProductID = addr;
-    myproduct[addr].ProductName = _ProductName;
-    myproduct[addr].ProductType = _ProductType;
-    myproduct[addr].ProductPrice = _ProductPrice;
-    myproduct[addr].state = ProductState.SETTING;
-
-    ProductAddresses.push(addr);
-}
-
-function getAllProductAddr() external view returns (address[] memory) {
-    return ProductAddresses;
-}
-
-
-function getProduct(address id) public view returns (address adrr,string memory _ProductName,string memory _ProductType, uint _ProductPrice,ProductState _state)
-    {
-       
-        Product memory p = myproduct[id];
-        
-        
-        return (p.ProductID, p.ProductName, p.ProductType, p.ProductPrice,p.state);
     }
-function get() public view returns (uint256) {
-        return test;
-}
+    mapping (address => Product) myProduct;
+    address[] public ProductAddresses;
+
+    function createProduct(string memory _ProductName,string memory _ProductType, uint _ProductPrice) public {
+        address addr = address(bytes20(sha256(abi.encodePacked(msg.sender,block.timestamp))));
+        myProduct[addr].ProductID = addr;
+        myProduct[addr].ProductName = _ProductName;
+        myProduct[addr].ProductType = _ProductType;
+        myProduct[addr].ProductPrice = _ProductPrice;
+        myProduct[addr].state = ProductState.SETTING;
+
+        ProductAddresses.push(addr);
+    }
+
+    function getAllProductAddr() external view returns (address[] memory) {
+        return ProductAddresses;
+    }
+
+
+    function getProduct(address id) public view returns (address adrr,string memory _ProductName,string memory _ProductType, uint _ProductPrice,ProductState _state)
+        {
+        
+            Product memory p = myProduct[id];
+            
+            
+            return (p.ProductID, p.ProductName, p.ProductType, p.ProductPrice,p.state);
+        }
+    function get() public view returns (uint256) {
+            return test;
+    }
 
 }
