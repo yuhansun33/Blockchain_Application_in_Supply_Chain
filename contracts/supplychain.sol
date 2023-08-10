1decdc89f3ba287808a73eba5bd7cb1432e80bcd
Linszuchi047 <140520487+Linszuchi047@users.noreply.github.com>
2023-08-10 20:06:24 +0800

    更新!

--- contracts/supplychain.sol
+++ contracts/supplychain.sol
@@ -1,8 +1,7 @@
-// SPDX-License-Identifier: GPL-3.0
+//SPDX-License-Identifier: MIT
 
-pragma solidity >=0.5.0 <0.9.0;
-
-enum ProductState{
+pragma solidity >=0.7.0 <0.9.0;
+enum ProductState {
     SETTING,
     PROCESSING,
     FINISH,
@@ -10,45 +9,92 @@ enum ProductState{
     ARRIVED
 }
 
-contract SupplyChain{
-    uint test = 10;
-    struct Product{
+contract SupplyChain {
+    struct Product {
+        address Manufacturer;
         address ProductID;
         string ProductName;
         string ProductType;
         uint ProductPrice;
+        uint TimeStamp;
         ProductState state;
-        
     }
-    mapping (address => Product) myProduct;
-    address[] public ProductAddresses;
+    mapping(address => Product) myproduct;
+    address[] ProductAddresses;
+    Product[] products;
+    string Manufacturer_Address;
 
-    function createProduct(string memory _ProductName,string memory _ProductType, uint _ProductPrice) public {
-        address addr = address(bytes20(sha256(abi.encodePacked(msg.sender,block.timestamp))));
-        myProduct[addr].ProductID = addr;
-        myProduct[addr].ProductName = _ProductName;
-        myProduct[addr].ProductType = _ProductType;
-        myProduct[addr].ProductPrice = _ProductPrice;
-        myProduct[addr].state = ProductState.SETTING;
+    function createProduct(
+        string memory _ProductName,
+        string memory _ProductType,
+        uint _ProductPrice
+    ) public {
+        address addr = address(
+            bytes20(sha256(abi.encodePacked(msg.sender, block.timestamp)))
+        );
+        myproduct[addr].Manufacturer = msg.sender;
+        myproduct[addr].ProductID = addr;
+        myproduct[addr].ProductName = _ProductName;
+        myproduct[addr].ProductType = _ProductType;
+        myproduct[addr].ProductPrice = _ProductPrice;
+        myproduct[addr].TimeStamp = block.timestamp;
+        myproduct[addr].state = ProductState.SETTING;
 
         ProductAddresses.push(addr);
+        products.push(
+            Product(
+                msg.sender,
+                addr,
+                _ProductName,
+                _ProductType,
+                _ProductPrice,
+                block.timestamp,
+                ProductState.SETTING
+            )
+        );
+    }
+
+    function enterAddress(string memory _Manufacturer_Address) public {
+        Manufacturer_Address = _Manufacturer_Address;
+    }
+
+    function getManufacturerAddress() public view returns (string memory) {
+        return Manufacturer_Address;
     }
 
     function getAllProductAddr() external view returns (address[] memory) {
         return ProductAddresses;
     }
 
-
-    function getProduct(address id) public view returns (address adrr,string memory _ProductName,string memory _ProductType, uint _ProductPrice,ProductState _state)
-        {
-        
-            Product memory p = myProduct[id];
-            
-            
-            return (p.ProductID, p.ProductName, p.ProductType, p.ProductPrice,p.state);
-        }
-    function get() public view returns (uint256) {
-            return test;
+    function getAllProduct() public view returns (Product[] memory) {
+        return products;
     }
 
+    function getProduct(
+        address id
+    )
+        public
+        view
+        returns (
+            address _Manufacturer,
+            address adrr,
+            string memory _ProductName,
+            string memory _ProductType,
+            uint _ProductPrice,
+            uint _TimeStamp,
+            ProductState _state
+        )
+    {
+        Product memory p = myproduct[id];
+
+        return (
+            p.Manufacturer,
+            p.ProductID,
+            p.ProductName,
+            p.ProductType,
+            p.ProductPrice,
+            p.TimeStamp,
+            p.state
+        );
+    }
 }
