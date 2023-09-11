c3dd37514e5dc66b71ee50d0d7ab46a80d1a9e8d
Linszuchi047 <140520487+Linszuchi047@users.noreply.github.com>
2023-09-11 16:50:17 +0800

    change

--- truffle-config.js
+++ truffle-config.js
@@ -65,9 +65,9 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
     },
     //
     // An additional network, but with some advanced options…
@@ -107,7 +107,7 @@ module.exports = {
   contracts_build_directory: "./client/src/contracts",
   compilers: {
     solc: {
-      version: "0.8.17",      // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.10",      // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
